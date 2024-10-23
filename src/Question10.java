d6927501985531601ab4cb16e6b972764ec866db
tjsgu <202230106@email.daelim.ac.kr>
2024-10-23 16:49:43 +0900

    commit2

---
+++ src/Question10.java
@@ -0,0 +1,12 @@
+public class Question10 {
+    public static void main(String[] args) {
+        int[] a = {20,30,50,70,90};
+        double b = 0;
+
+        for(int i=0;i<a.length;i++) {
+            b += a[i];
+
+        }
+        System.out.println(b/5);
+    }
+}
